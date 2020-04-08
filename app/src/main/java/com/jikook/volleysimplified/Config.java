8128bf1a9a9ef57642526df31c07890344a9ab5a
RkRabbit <emailtork09@gmail.com>
2020-04-08 18:47:15 +0530

    Initial commit

---
+++ app/src/main/java/com/jikook/volleysimplified/Config.java
@@ -0,0 +1,5 @@
+package com.jikook.volleysimplified;
+
+public class Config {
+    public static final String URL = "http://localhost/sample/demo";
+}
--- app/src/main/java/com/jikook/volleysimplified/MainActivity.java
+++ app/src/main/java/com/jikook/volleysimplified/MainActivity.java
@@ -1,8 +1,13 @@
 package com.jikook.volleysimplified;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.rabbitt.simplyvolley.ServerCallback;
+import com.rabbitt.simplyvolley.VolleyAdapter;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HashMap<String, String> map = new HashMap<>();
+        //..Put your Hashmap values here.
+
+
+        //GetData method will get the data from the particular url
+        new VolleyAdapter(this).getData(Config.URL, new ServerCallback() {
+            @Override
+            public void onSuccess(String result) {
+                //result can be converted in to the JSONarray
+                //using JSONObject we can parse the data from the JSONarray
+            }
+
+            @Override
+            public void onError(String result) {
+
+            }
+        });
+
+        //Overloaded getdata method can be used to get the data by sending
+        //quering values in the Hashmap
+        new VolleyAdapter(this).getData(map, Config.URL, new ServerCallback() {
+            @Override
+            public void onSuccess(String result) {
+                //result can be converted in to the JSONarray
+                //using JSONObject we can parse the data from the JSONarray
+            }
+
+            @Override
+            public void onError(String result) {
+
+            }
+        });
+
+        new VolleyAdapter(this).insertData(map, Config.URL, new ServerCallback() {
+            @Override
+            public void onSuccess(String result) {
+                //result can be converted in to the JSONarray
+                //using JSONObject we can parse the data from the JSONarray
+            }
+
+            @Override
+            public void onError(String result) {
+
+            }
+        });
+
+
 
     }
 }
