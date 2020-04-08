ca889e32ca4fe8d376dde6e8825ea3c82133d444
RkRabbit <emailtork09@gmail.com>
2020-04-08 17:20:25 +0530

    Initial commit

---
+++ app/src/main/java/com/jikook/volleysimplified/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jikook.volleysimplified;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
